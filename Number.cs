ffa067dc355ee5b54427ec2df89fdb00a011b01e
Fauzzzs7 <fauzsyabana68@gmail.com>
2022-04-07 23:13:06 +0700

    latihan overloading method

---
+++ Number.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    internal class Numbers
+    {
+        //overloading method
+        public int FindMinimum(int number1, int number2)
+        {
+            int minim;
+
+            if (number1 < number2)
+            {
+                minim = number1;
+            } else
+            {
+                minim = number2;
+            }
+            return minim;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            int maxim;
+
+            if( number1 > number2)
+            {
+                maxim = number1;
+            } else
+            {
+                maxim = number2;
+            }
+            return maxim;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            int maximum;
+
+            if((number1 > number2) &&(number1 > number3))
+            {
+                maximum = number1;
+            } else if((number2 > number3) && (number2 > number1))
+            {
+                maximum = number2;
+            } else
+            {
+                maximum = (int)number3;
+            }
+
+            return maximum;
+        }
+
+    }
+}
